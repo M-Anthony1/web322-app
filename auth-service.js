f918cca5d8e66472c2ef974d49c57112c2b7e905
Marco Schiralli <mschiralli1@myseneca.ca>
2022-08-05 23:34:06 -0400

    second commit

--- auth-service.js
+++ auth-service.js
@@ -3,8 +3,7 @@ const bcrypt = require("bcryptjs")
 var Schema = mongoose.Schema
 
 var userSchema = new Schema({
-    "userName": {
-        
+    "userName": { 
         type: String,
         unique: true
     }, 
@@ -30,68 +29,59 @@ var userSchema = new Schema({
    }
 
 
-module.exports.registerUser = function (userData) {
+module.exports.registerUser = function (userData){
+    return new Promise((resolve,reject) => {
 
-    return new Promise((resolve, reject) => {
-
-        if (userData.password !== userData.password2){
-            
-            reject("Passwords do not match")
-        }
-
-        bcrypt.hash(userData.password, 10).then(hash => {
-            userData.password = hash
-            let newUser = new User(userData)
-
-            newUser.save().then(() => {
-                
-                resolve()
-            }).catch(err =>{
-
-                    if (err.code == 11000)
-                    {
-                        reject("User Name already taken");
-                    }
-                    else 
-                    {
-                        reject(`There was an error creating the user: ${err}`);
-                    }
-                })
-               
-            })
+        if(userData.password != userData.password2){
+            reject("Passwords do not match");
+        } 
         
-        }).catch(error =>{
-            reject(`There was an error encrypting the password ${error}`);
-        });
+            bcrypt.hash(userData.password, 10).then(hash=>{
+                userData.password = hash;
+            let newUser = new User(userData);
 
-    }
+            newUser.save().then(()=>{
+            resolve()
+            
+             }).catch(err=>{
 
+                if(err.code == 11000){
+                    reject("User Name already taken");
 
-
+                }else{
+                    reject(`There was an error creating the user: ${err}`);
+                }
+            })
+        }).catch(err=>{
+            reject(`There was an error encrypting the password ${err}`);
+        })
+        
+    })
+}
 module.exports.checkUser = function(userData) {
     return new Promise((resolve, reject) => {
         User.find({ userName: userData.userName })
             .exec()
             .then(users => {
-                bcrypt.compare(userData.password, users[0].password).then(res => {
-                    if (res === true) {
-                        users[0].loginHistory.push({ dateTime: (new Date()).toString(), userAgent: userData.userAgent });
-                        User.update(
-                            { userName: users[0].userName },
-                            { $set: { loginHistory: users[0].loginHistory } },
-                            { multi: false }
+                bcrypt.compare(userData.password, users[0].password).then(result => {
+                    if (result) {
+                        users[0].loginHistory.push({ dateTime: (new Date()).toString(), userAgent: userData.userAgent })
+                        User.updateOne(
+                            { userName: userData.userName },
+                            { $set: { loginHistory: users[0].loginHistory } }
+                        
                         )
                             .exec()
-                            .then(() => { resolve(users[0]) })
-                            .catch(err => { reject("There was an error verifying the user: " + err) })
+                            .then(() => { resolve(users[0])})
+                            .catch(err => { reject(`There was an error verifying the user: ${err}`)})
                     }
                     else {
-                        reject("Incorrect Password for user: " + userData.userName);
+                        reject(`Incorrect Password for user: ${userData.userName}`);
                     }
                 })
             })
             .catch(() => {
-                reject("Unable to find user: " + userData.userName);
+                reject(`Unable to find user: ${userData.userName}`);
             })
     })
 
